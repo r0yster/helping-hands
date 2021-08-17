e3829cba18b1adfd4068ca65e85e7ff8dfcbd033
Rachel Solov <rsolov23@yahoo.com>
2021-08-17 16:38:02 -0500

    initial frontend file setup, worked on login and signup components

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import LoginForm from "../src/components/LoginForm";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+import SignupForm from "./components/SignupForm";
+import Dashboard from "./pages/Dashboard";
+import Nav from "./components/Nav";
+function App() {
+  const [currentPage, handlePageChange] = useState("Dashboard");
+  const renderPage = () => {
+    switch (currentPage) {
+      case "Signup":
+        return <SignupForm />;
+      case "Login":
+        return <LoginForm />;
+      case "Dashboard":
+        return <Dashboard />;
+
+      default:
+        return <Dashboard />;
+    }
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ChakraProvider>
+          <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+          <div>{renderPage()}</div>
+        </ChakraProvider>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Dashboard.css";
+function Dashboard() {
+  return (
+    <div>
+      <h1 className="header">Chill Pill Dashboard</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
