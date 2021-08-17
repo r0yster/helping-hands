8602ba96f4dd939272a2898e153b8ad5a445873f
Lex Crawford <lexcraw4d@gmail.com>
2021-08-16 23:49:02 -0400

    backend file structure User signin JWT made

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,8 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '1d';
 
 module.exports = {
   // function for our authenticated routes
