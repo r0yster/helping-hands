7a5019933a696d2fbd24b4a39f5b6c922008e13c
Rachel Solov <rsolov23@yahoo.com>
2021-08-20 18:37:08 -0500

    worked on about component and added apolloprovider to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 import LoginForm from "./components/LoginForm/LoginForm";
 import "../src/index.css";
 // 1. import `ChakraProvider` component
@@ -12,21 +18,31 @@ import Aboutus from "./components/Aboutus/Aboutus";
 // import Nav from "./components/Nav";
 import Header from "./components/Header/Header";
 
+const httpLink = createHttpLink({
+  uri: "http://localhost:3001/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 function App() {
   return (
-    <BrowserRouter>
-      <ChakraProvider>
-        <Header />
-        <Switch>
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/signup" component={SignupForm} />
-          <Route exact path="/login" component={LoginForm} />
-          <Route exact path="/event" component={Event} />
-          <Route exact path="/volunteer" component={Volunteer} />
-          <Route exact path="/aboutus" component={Aboutus} />
-        </Switch>
-      </ChakraProvider>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <ChakraProvider>
+          <Header />
+          <Switch>
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/signup" component={SignupForm} />
+            <Route exact path="/login" component={LoginForm} />
+            <Route exact path="/event" component={Event} />
+            <Route exact path="/volunteer" component={Volunteer} />
+            <Route exact path="/aboutus" component={Aboutus} />
+          </Switch>
+        </ChakraProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/Aboutus/Aboutus.js
+++ client/src/components/Aboutus/Aboutus.js
@@ -1,6 +1,71 @@
+import { Flex, Box, Stack, chakra, Text } from "@chakra-ui/react";
 import React from "react";
+import { FaUserAlt, FaLock } from "react-icons/fa";
+// import backgroundImage from "../images/background.jpg";
 
 function Aboutus() {
-  return <div className="about"> {/* <h1>About Us</h1>{" "} */}</div>;
+  return (
+    <Flex
+      flexDirection="column"
+      width="100wh"
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
+      backgroundImage="url('../images/background.jpg')"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+      opacity="63%"
+    >
+      <Stack
+        flexDir="column"
+        mb="8rem"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box minW={{ base: "90%", md: "468px" }}>
+          <Text fontSize="5xl" alignItems="center" color="purple.50">
+            Help get food to those in
+          </Text>
+          <Text
+            fontSize="5xl"
+            alignItems="center"
+            marginBottom="5rem"
+            color="purple.50"
+          >
+            need in your community.
+          </Text>
+
+          <Box>
+            <Flex
+              align="center"
+              justify="center"
+              color="purple.50"
+              fontSize="2em"
+            >
+              Our Mission
+            </Flex>
+          </Box>
+        </Box>
+        Our Mission
+        <Flex
+          align="center"
+          justify="center"
+          fontSize="1.5em"
+          color="purple.50"
+        >
+          "" seeks to serve the less fortunate through
+        </Flex>
+        <Flex
+          align="center"
+          justify="center"
+          fontSize="1.5em"
+          color="purple.50"
+        >
+          grass roots efforts of engaged community members.
+        </Flex>
+      </Stack>
+    </Flex>
+  );
 }
 export default Aboutus;
